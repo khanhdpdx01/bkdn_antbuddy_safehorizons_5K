d2ab15a2efd3963185759d21a42c4ffbbceb7abb
khanhdpdx01 <khanhdpdx@gmail.com>
2021-12-30 14:32:27 +0700

    Create database

--- packages/backend/src/database/migrations/20211230053359_categories.js
+++ packages/backend/src/database/migrations/20211230053359_categories.js
@@ -4,7 +4,6 @@ exports.up = (knex) => knex.schema.createTable('categories', (table) => {
     table.text('description').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-  // table.uuid('id').primary();
 });
 
 exports.down = (knex) => knex.schema.dropTable('categories');
